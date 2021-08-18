b34fb37409c6fe731fae5bc3a2bd5a2dbb1b3156
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2021-08-18 16:35:37 -0400

    Create peso_ideal.cpp

---
+++ 1 - Introdução à linguagem C e C++/Lista de exercícios para praticar C e C++/Everton Oliveira/peso_ideal.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int main(){
+
+
+
+    int sexo, repeticao;
+    float altura, calculo;
+    
+    while(true){
+    printf("Digite a quantidade de pessoas: ");
+    scanf("%d", &repeticao);
+    if(repeticao>0){
+        break;
+        }
+    }
+        while(repeticao>0){
+        while(true){
+            printf("Digite [1] homem\nDigite [2] mulher: ");
+            scanf("%d", &sexo);
+            if (sexo == 1 || 2){
+                break;
+            }  
+        }
+        while(true){
+            printf("Digite a altura em metro: ");
+            scanf("%f", &altura);
+            if(altura>0){
+                break;
+            }
+        }
+
+        if (sexo == 1){
+            
+            calculo = (72.7*altura)-58;
+            printf("Seu peso ideal deve ser igual a: %.2f\n", calculo);
+
+        }else
+        {
+            
+            calculo = (62.1*altura)-44.7;
+            printf("Seu peso ideal deve ser igual a: %.2f\n", calculo);
+        }
+        repeticao = repeticao - 1;
+    }
+    return 0;
+}
