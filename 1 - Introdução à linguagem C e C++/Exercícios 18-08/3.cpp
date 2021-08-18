07acc951bd578c9ad8cb0c779ac82688fbdafbbd
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2021-08-18 16:02:53 -0400

    Create 3.cpp

---
+++ 1 - Introdução à linguagem C e C++/Exercícios 18-08/3.cpp
@@ -0,0 +1,69 @@
+/*Programa lê duas matrizes A e B de dimensões mxn e calcula a matriz resultante da
+soma de A e B*/
+#include<stdio.h>
+# define MAX 10
+
+/*protótipo das funções*/
+void leitura(int M[MAX][MAX], int m, int n);
+void impressao(int M[MAX][MAX], int m, int n);
+void soma(int A[MAX][MAX], int B[MAX][MAX], int C[MAX][MAX], int m, int n);
+
+/*Função principal*/
+int main()
+{
+	int A[MAX][MAX], B[MAX][MAX], C[MAX][MAX];
+	int m,	// número de linhas 
+		n;	// número de colunas
+	int i, j;
+	
+	printf("Digite número de linhas e colunas: ");
+	scanf("%d%d", &m,&n);
+	printf("\n");
+
+	leitura(A, m, n);
+	leitura(B, m, n);
+	soma(A, B, C, m, n);
+	impressao(C, m, n);
+
+	return 0;
+}
+
+/*Função para armazenar dados na matriz inteira M de dimensoes m por n*/
+void leitura(int M[MAX][MAX], int m, int n)
+{
+	int i, j;
+
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			scanf("%d", &M[i][j]);
+		}
+	}
+}
+/*Função para imprimir as células da matriz inteira M de dimensoes m por n*/
+void impressao(int M[MAX][MAX], int m, int n)
+{
+	int i, j;
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			printf("%d ", &M[i][j]);
+		}
+		printf("\n");
+	}
+}
+/*Função calcula a soma das matrizes A e B de dimensões mxn e armazena o resultado
+na matriz C*/
+void soma(int A[MAX][MAX], int B[MAX][MAX], int C[MAX][MAX], int m, int n)
+{
+	int i, j;
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < n; j++)
+		{
+			C[i][j] = A[i][j] + B[i][j];
+		}
+	}
+}
