df1fed830e7b5b9ee9db82a1e6c4437b4fc17d42
Lourdes Oshiro Igarashi <83931936+LourdesOshiroIgarashi@users.noreply.github.com>
2021-09-30 18:07:37 -0400

    Create condicao_de_existencia.cpp

---
+++ 4 - Recursão/Atividade Avaliativa sobre Recursão/condicao_de_existencia.cpp
@@ -0,0 +1,6 @@
+/*
+Em um algoritmo que utilize a técnica da recursão, é necessária a existência de uma condição de parada a fim de se evitar que o algoritmo entre em um loop.
+
+Escolha uma opção:
+Verdadeiro
+*/
